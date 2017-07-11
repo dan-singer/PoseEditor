349c878e9b9658770c2da8bb3fb299da5fdde76e
Dan Singer <djs5435@g.rit.edu>
2017-07-11 16:44:26 -0400

    Refinements and additions to html buttons.

--- js/pose-editor.js
+++ js/pose-editor.js
@@ -3,8 +3,37 @@
 document.addEventListener("DOMContentLoaded", function(event){
     document.getElementById("gameContainer").style.margin = "auto";
     console.log("Correcting margin");
+    ApplyToolbarHandlers();
 });
 
+
+/**
+ * Applies click listeners to the toolbar buttons
+ */
+function ApplyToolbarHandlers(){
+    var buttons = document.getElementsByClassName("toolbar-button");
+    //Megaplay
+    buttons[0].addEventListener("click", function(){
+        gameInstance.SendMessage("RTClipEditor", "PlayActiveClipFromHTML");
+    });
+    //Play/pause
+    buttons[1].addEventListener("click", function(){
+        gameInstance.SendMessage("RTClipEditor", "TogglePause");
+    });
+    //Reset active curve
+    buttons[2].addEventListener("click", function(){
+        gameInstance.SendMessage("RTClipEditor", "ResetActiveCurve");
+    });
+    //Reset active clip
+    buttons[3].addEventListener("click", function(){
+        gameInstance.SendMessage("RTClipEditor", "ResetActiveClip");
+    });
+}
+
+
+function Export(){
+    gameInstance.SendMessage("RTClipEditor", "SaveActiveClip");
+}
 
 
 /**
